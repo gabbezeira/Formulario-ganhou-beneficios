ee371a2108947e7c2b5573d2893215795a788c7d
gabbezeira <gabrielalves.code@gmail.com>
2025-01-09 11:32:12 -0300

    ajustando SEO

--- src/styles/styles.jsx
+++ src/styles/styles.jsx
@@ -1,5 +1,5 @@
 import styled, { keyframes } from 'styled-components'
-import bgImage from '../assets/bg.jpg'
+import bgImage from '../assets/bg.webp'
 import './globalStyles.css'
 
 export const Container = styled.div`
